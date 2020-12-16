f05b1591d23706c9744703fec9851df05a92e930
L. Stubbs <technoexpressnet@gmail.com>
2020-12-16 14:08:26 -0500

    update/fix for a working Windows package installer

--- bin/installChocolatey.cmd
+++ bin/installChocolatey.cmd
@@ -1,6 +1,2 @@
 @echo off
-SET DIR=%~dp0%
-
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://chocolatey.org/install.ps1','install.ps1'))"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%install.ps1' %*"
-SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
